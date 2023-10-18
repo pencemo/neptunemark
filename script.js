8f5174e4866a75da11c249ba9b16a5428a8e8879
unknown <mnmsby4@gmail.com>
2023-10-18 12:50:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// const menuToggle = document.querySelector('.menu-toggle');
+// const barlink = document.querySelector('.bar-link');
+
+// menuToggle.addEventListener('click', () => {
+//     barlink.classList.toggle('show');
+// });
