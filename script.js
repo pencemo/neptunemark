4da92cffcbb99e5d7294f648b13686461f86651d
unknown <mnmsby4@gmail.com>
2023-10-19 16:40:21 +0530

    new edit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
     var typed = new Typed('.auto-type', {
-      strings: ['Dreams', 'Bussines'],
+      strings: ['Dreams', 'Business', 'Ideas','Brand','Vision' ],
       typeSpeed: 100,
       backSpeed: 50,
       loop: true,
