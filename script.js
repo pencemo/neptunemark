9aca302f01860ffbdd5316d75fe6829cd12ff5f0
unknown <mnmsby4@gmail.com>
2023-10-20 12:00:43 +0530

    add loader and fade in animation

--- script.js
+++ script.js
@@ -6,4 +6,15 @@
       loop: true,
       smartBackspace: true,
     });
+
+
+    window.addEventListener("load", () => {
+      const loder = document.querySelector(".loder");
+
+      loder.classList.add("loder--hide");
+
+      loder.addEventListener("transitionend", () => {
+        document.body.removeChild(load);
+      });
+    });
  
