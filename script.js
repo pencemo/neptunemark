ca8171c6825502e0c9068ccc9dc8bb9eaec0e904
unknown <mnmsby4@gmail.com>
2023-10-21 19:31:37 +0530

    add new page about

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// type animation
     var typed = new Typed('.auto-type', {
       strings: ['Dreams', 'Business', 'Ideas','Brand','Vision' ],
       typeSpeed: 100,
@@ -6,15 +6,43 @@
       loop: true,
       smartBackspace: true,
     });
+//--------------------------//  
 
 
+// load animation
     window.addEventListener("load", () => {
       const loder = document.querySelector(".loder");
 
       loder.classList.add("loder--hide");
 
       loder.addEventListener("transitionend", () => {
-        document.body.removeChild(load);
+        document.body.removeChild(load)
       });
     });
- 
+
+//--------------------------// 
+
+
+
+// sroll fade animation cdn 
+   
+      AOS.init({
+        duration: 1000
+      });
+//--------------------------//     
+
+
+// toggle animation
+
+      $(document).ready(function () {
+        $("#icon").click(function () {
+          $("ul").toggleClass("show");
+        });
+        $("#clos1").click(function () {
+          $("ul").removeClass("show");
+        });
+      });
+
+     
+//--------------------------//
+
