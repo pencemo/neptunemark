5580cc332a9c399002a33916b703e6e715fae77d
unknown <mnmsby4@gmail.com>
2023-10-27 18:20:04 +0530

    add slide card

--- script.js
+++ script.js
@@ -61,3 +61,20 @@
 });
 
 //jaquary end------
+
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay:{
+    delay:2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
